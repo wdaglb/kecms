d95d798525e1c0ff8d0807c5d8689a9758e9948d
King east <1207877378@qq.com>
2017-08-18 11:38:39 +0800

    Signed-off-by: King east <1207877378@qq.com>

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * KECMS - 综合内容管理系统
+ * @domain  http://cms.iydou.cn/
+ * @authors King east (1207877378@qq.com)
+ */
+
+return [
+	'host'=>'127.0.0.1',
+	'name'=>'test',
+	'user'=>'root',
+	'pass'=>'root',
+	'charset'=>'utf-8',
+	'prefix'=>'ke_',
+	'port'=>3306
+];
